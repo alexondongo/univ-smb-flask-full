d95661344242f9074d713b2dab58fb02ada5d92b
alexondongo <95148501+alexondongo@users.noreply.github.com>
2023-03-30 23:49:10 +0200

    Update MariaDB.sh

--- manifest/MariaDB.sh
+++ manifest/MariaDB.sh
@@ -12,7 +12,7 @@ sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('ondongoa') WHERE User 
 echo "ok1"
 sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'ondongoa' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 echo "ok2"
-sudo mysql -e "create database identity; create database config_generator;"
+mysql -u root -pondongoa -e "create database identity; create database config_generator;"
 mysql -u root -pondongoa -e "use identity; create table users(user_id INTEGER auto_increment PRIMARY KEY, user_nom VARCHAR(255) not null, user_prenom VARCHAR(255) not null, user_date DATE not null)ENGINE=InnoDB; create table usernames(user_id INTEGER auto_increment PRIMARY KEY, username VARCHAR(255) not null, password VARCHAR(255) not null)ENGINE=InnoDB;"
 echo "ok1"
 mysql -u root -pondongoa -e "use config_generator; CREATE TABLE webserver (id INT(11) NOT NULL AUTO_INCREMENT, root_path VARCHAR(255) NOT NULL, index_files VARCHAR(255) NOT NULL, error_page VARCHAR(255) NOT NULL, error_page_path VARCHAR(255) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB; CREATE TABLE reverse_proxy (id INT(11) NOT NULL AUTO_INCREMENT, strategy_method VARCHAR(255) NOT NULL, server1_domain VARCHAR(255) NOT NULL, server2_domain VARCHAR(255) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB; CREATE TABLE load_balancer (id INT(11) NOT NULL AUTO_INCREMENT, app_path VARCHAR(255) NOT NULL, proxy_bind VARCHAR(255) NOT NULL, proxy_pass VARCHAR(255) NOT NULL, webserver_id INT(11) NOT NULL, PRIMARY KEY (id), CONSTRAINT fk_load_balancer_webserver FOREIGN KEY (webserver_id) REFERENCES webserver(id)) ENGINE=InnoDB;"
